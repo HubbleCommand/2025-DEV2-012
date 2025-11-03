1105a562a8f2c35a27d21855c4cc7e3cc26484bc
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2025-11-03 23:54:14 +0100

    ui shapes & layout
    + use shapes for Xs and Os
    + centre current player text correctly & move to bottom

--- app/src/main/java/com/dev2012/tictactoe/ui/TicTacToeView.kt
+++ app/src/main/java/com/dev2012/tictactoe/ui/TicTacToeView.kt
@@ -4,6 +4,7 @@ package com.dev2012.tictactoe.ui
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -23,12 +24,19 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.graphics.shapes.RoundedPolygon
+import androidx.graphics.shapes.circle
+import androidx.graphics.shapes.toPath
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.dev2012.tictactoe.R
 
@@ -48,9 +56,33 @@ fun TicTacToeElement(
             }
         },
         modifier = modifier
-    ) {
-        Text("$value")
-    }
+            //drawWithCache could be used... but we aren't doing anything heavy here
+            .drawBehind {
+                if (value == Play.None) {
+                    return@drawBehind
+                }
+
+                val pth = when (value) {
+                    Play.O -> {
+                        val circlePolygon = RoundedPolygon.circle(
+                            numVertices = 64,
+                            radius = size.minDimension / 2f - 12f, centerX = size.width / 2f, centerY = size.height / 2f
+                        )
+                        circlePolygon.toPath().asComposePath()
+                    }
+                    Play.X -> Path().apply {
+                        moveTo(0f, 0f)
+                        lineTo(size.minDimension, size.minDimension)
+                        moveTo(size.minDimension, 0f)
+                        lineTo(0f, size.minDimension)
+                    }
+                    else -> null
+                }
+                pth?.let {
+                    drawPath(pth, color = Color.White, style = Stroke(width = 12f))
+                }
+            }
+    ) { }
 }
 
 @Composable
@@ -98,10 +130,13 @@ fun TicTacToeGameView(modifier: Modifier, vm: TicTacToeViewModel = viewModel()) 
     ) {
         //As grid is fixed size, don't need lazy anything
         // while it will always be a 3x3 grid, I don't like hard-coding values like that
-        Column {
-            Text("${stringResource(R.string.player)}: ${state.currentPlayer}", textAlign = TextAlign.Center)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             repeat(state.grid.size) { col ->
-                Row {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
                     repeat(state.grid[col].size) { row ->
                         TicTacToeElement(
                             Modifier
@@ -119,6 +154,8 @@ fun TicTacToeGameView(modifier: Modifier, vm: TicTacToeViewModel = viewModel()) 
                     Modifier.width(itemSize * state.grid[0].size), color = Color.Gray, thickness = 1.dp
                 )
             }
+            Spacer(modifier = Modifier.size(30.dp))
+            Text("${stringResource(R.string.player)}: ${state.currentPlayer}", textAlign = TextAlign.Center)
         }
     }
 }
